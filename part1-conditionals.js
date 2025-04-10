aab8e9254000401222d71c4207b7cd989224fb2e
Shelby Norris <shelbs9109@gmail.com>
2025-04-10 09:26:46 -0500

    Created index html file, created 3 separate js files and linked to html

---
+++ part1-conditionals.js
@@ -0,0 +1,17 @@
+console.log("Hello World!")
+
+//Grade Checker
+//Print A, B, C, D, or F based on score
+let score = 82;
+
+
+
+//Pizza Quantity Warning
+//Print warning if quantity > 10
+let quantity = 12;
+
+
+
+//Login Simulator
+//Print "Access Granted" or "Access Denied"
+let password = "letmein";

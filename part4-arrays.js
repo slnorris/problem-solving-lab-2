0d799977e0ae194b66bf2d4f1e45d42b1fd25aee
Shelby Norris <shelbs9109@gmail.com>
2025-04-17 20:04:41 -0500

    completed problem 4 and random picker

--- part4-arrays.js
+++ part4-arrays.js
@@ -1,41 +1,41 @@
-// // console.log("Hello World!")
+// console.log("Hello World!")
 
-// // Problem 1: Favorite Foods
+// Problem 1: Favorite Foods
 
-// let favFoods = ["tacos", "sushi", "pizza", "pasta", "chicken"];
-// console.log(favFoods);
+let favFoods = ["tacos", "sushi", "pizza", "pasta", "chicken"];
+console.log(favFoods);
 
-// console.log(favFoods.at(0));
+console.log(favFoods.at(0));
 
-// console.log(favFoods.at(4));
+console.log(favFoods.at(4));
 
-// favFoods.push("Watermelon");
+favFoods.push("Watermelon");
 
-// console.log(favFoods);
+console.log(favFoods);
 
-// // Problem 2: Guest List
+// Problem 2: Guest List
 
-// let guestList = [
-//   "Frank",
-//   "Monica",
-//   "Fiona",
-//   "Phillip",
-//   "Ian",
-//   "Debbie",
-//   "Carl",
-//   "Liam",
-//   "Veronica",
-//   "Kevin",
-//   "Mickey",
-// ];
+let guestList = [
+  "Frank",
+  "Monica",
+  "Fiona",
+  "Phillip",
+  "Ian",
+  "Debbie",
+  "Carl",
+  "Liam",
+  "Veronica",
+  "Kevin",
+  "Mickey",
+];
 
-// guestList.shift();
+guestList.shift();
 
-// guestList.push("Karen");
+guestList.push("Karen");
 
-// console.log(guestList);
+console.log(guestList);
 
-// console.log(guestList.length);
+console.log(guestList.length);
 
 // Problem 3: Weekend Tasks
 
@@ -48,3 +48,30 @@ function printTasks(tasks) {
 }
 
 printTasks(tasks)
+
+// Problem 4: Reverse It (Manual)
+
+let numbers = [1, 2, 3, 4, 5];
+
+for (let i = numbers.length - 1; i >= 0; i--) {
+  console.log(numbers[i]);
+}
+
+// Bonus: Random Picker
+
+let lunches = [
+  "sandwich",
+  "salad",
+  "burrito",
+  "sushi",
+  "soup",
+  "pasta",
+  "chicken",
+];
+
+function randomLunch(lunches) {
+    let lunchToday = Math.floor(Math.random() * lunches.length);
+    console.log(lunches[lunchToday])
+}
+
+randomLunch(lunches)
